80b08ca9ecf175e6046c92989a65ba6e4a27c4de
Stephanie Rohrdanz <stephanierohrdanz@Stephanie-Rohrdanzs-FTS-iMac.local>
2014-07-29 15:37:17 -0400

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,71 +1,95 @@
-$(document).ready(function(){
+(function(){
 
-if(!Modernizr.canvas) {
-	$.getScript( "assets/js/lib/jquery.form-validator.min.js" ).done(function() {
+if(Modernizr.input.required) {
+
+	var script = document.createElement("SCRIPT");
+    script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js';
+    script.type = 'text/javascript';
+    document.getElementsByTagName("head")[0].appendChild(script);
+
+     var checkReady = function(callback) {
+        if (window.jQuery) {
+            callback(jQuery);
+        }
+        else {
+            window.setTimeout(function() { checkReady(callback); }, 100);
+        }
+    };
+
+    checkReady(function($) {
+        $.getScript( "assets/js/lib/jquery.form-validator.min.js" ).done(function() {
 		$.validate({
+			validateOnBlur: false,
 			errorMessagePosition: $("#errors"),
-			onError : function() {
-			// $(".error:first").focus();
-			},
 			onSuccess : function() {
 				alert('The form is valid!');
 			}
 		});
+
+		$('input, select').on('validation', function(evt, isValid) {
+			var test = ((isValid ? 'VALID' : 'NOT VALID'));
+			if (test == "VALID") {      	
+				$(this).next(".circle").removeClass("nosuccess");
+				$(this).next(".circle").addClass("success");
+			} else if (test == "NOT VALID") {
+				$(this).next(".circle").removeClass("success");
+				$(this).next(".circle").addClass("nosuccess");
+			}
+		});	
+	});
 	});
 }
 
-$('input, select').on('validation', function(evt, isValid) {
-	var test = ((isValid ? 'VALID' : 'NOT VALID'));
-	if (test == "VALID") {      	
-		$(this).next(".circle").removeClass("nosuccess");
-		$(this).next(".circle").addClass("success");
-		$(this).attr({"aria-invalid" : false});
-	} else if (test == "NOT VALID") {
-		$(this).next(".circle").removeClass("success");
-		$(this).next(".circle").addClass("nosuccess");
-		$(this).attr({"aria-invalid" : true});
-	}
-});
+var stylesheet = document.styleSheets;
+var radios = document.getElementsByName("cardtype");
+var card = document.getElementById("card");
+var sprite = document.getElementById("sprite");
+var sprite2 = document.getElementById("sprite2");
 
-var stylesheet = $("link[rel='stylesheet']").attr("href");
-
-console.log(stylesheet);
-
-if (stylesheet == "assets/css/styles.css") {
-	$("input[type='radio']").hide();
-	$("input[type='radio']").next('label').hide();
+if (stylesheet !== "") {
+	for( i = 0; i < radios.length; i++ ) {
+        radios[i].style.display = 'none';
+        radios[i].nextSibling.style.display = 'none';
+    }
 }
 
-$("#card").keyup(function(){
-	var cardcheck = $(this).val();
-	// visa
+function cardcheck() {
+	var cardcheck = card.value;
+	console.log(cardcheck);
+
 	if(cardcheck.match(/^4/)) {
-		$("#sprite").removeAttr("class");
-		$("#sprite").addClass("visa");
-		$("#r_visa").prop("checked", true);
+		sprite.removeAttribute("class");
+		sprite.setAttribute("class","visa");
+		document.getElementById("r_visa").checked=true;
 	// mastercard
 	} else if(cardcheck.match(/^5[1-5]/)) {
-		$("#sprite").removeAttr("class");
-		$("#sprite").addClass("mc");
-		$("#r_mc").prop("checked", true);
+		sprite.removeAttribute("class");
+		sprite.setAttribute("class","mc");
+		document.getElementById("r_mc").checked=true;
 	// discover
 	} else if(cardcheck.match(/^6/)) {
-		$("#sprite").removeAttr("class");
-		$("#sprite").addClass("disc");
-		$("#r_disc").prop("checked", true);
+		sprite.removeAttribute("class");
+		sprite.setAttribute("class","disc");
+		document.getElementById("r_disc").checked=true;
 	// amex
 	} else if(cardcheck.match(/^3[4]|[7]/)) {
-		$("#sprite").removeAttr("class");
-		$("#sprite").addClass("amex");
-		$("#sprite2").addClass("amex2");
-		$("#r_amex").prop("checked", true);
+		sprite.removeAttribute("class");
+		sprite.setAttribute("class","amex");
+		sprite2.setAttribute("class","amex2");
+		document.getElementById("r_amex").checked=true;
 	} else {
 	// all cards 
-		$("#sprite").removeAttr("class");
-		$("#sprite").addClass("allcards");
-		$("#sprite2").removeClass("amex2");
-		$("input[type='radio']").prop("checked", false);
+		sprite.removeAttribute("class");
+		sprite2.removeAttribute("class");
+		sprite2.setAttribute("class","allcards");
+    		for( i = 0; i < radios.length; i++ ) {
+	        	if(radios[i].checked) {
+	            radios[i].checked=false;
+	        }
+    }
 	}
-});
+}
 
-});
+card.addEventListener("keyup", cardcheck, false);
+
+})();
